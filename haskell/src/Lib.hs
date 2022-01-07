a3ee628a907d0da91c6d7b763471af95210960b3
seaerchin <jer005@e.ntu.edu.sg>
2022-01-08 02:15:18 +0800

    feat(main): add arg parsing

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Options.Applicative (Alternative ((<|>)), Parser, execParser, helper, (<**>))
+import Options.Applicative.Builder
+
+data Entry = File String | Prompt deriving (Show, Eq)
+
+-- utility functions for easy lookup
+todo :: String -> a
+todo msg = error ("TODO: " ++ msg)
+
+-- exec is the main entrypoint into lox
+exec :: IO ()
+exec = execParser (info (combinedParser <**> helper) loxInfo) >>= todo "add implementation"
+  where
+    combinedParser = sourceFileParser <|> replParser -- we have to try the source file first as repl always succeeds
+    loxInfo = header "Welcome to the lox interpreter." <> fullDesc <> progDesc "Either pass a path to the source file or invoke as is to enter the REPL"
+
+sourceFileParser :: Parser Entry
+sourceFileParser = argument (File <$> str) (metavar "FILENAME")
+
+-- nothing to parse
+replParser :: Parser Entry
+replParser = pure Prompt
