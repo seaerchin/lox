034ee84d105298b488dc87f4729ea3d51c30a98b
seaerchin <jer005@e.ntu.edu.sg>
2022-01-10 21:56:09 +0800

    feat(scanner): added scanner module with basic error handling and newtype

---
+++ haskell/src/Lox/Scanner.hs
@@ -0,0 +1,34 @@
+module Lox.Scanner where
+
+import Relude
+import System.IO (hPutStrLn)
+import Util
+
+data Token = Token deriving (Eq, Show)
+
+data Position = Pos
+  { line :: Int,
+    col :: Int
+  }
+  deriving (Eq, Show)
+
+type ScannerState a = StateT Position IO a
+
+getScannerLine :: ScannerState Int
+getScannerLine = line <$> get
+
+getScannerCol :: ScannerState Int
+getScannerCol = col <$> get
+
+scanTokens :: Show s => s -> [Token]
+scanTokens = todo "convert the source string into a list of tokens"
+
+error :: Text -> ScannerState ()
+error message = do
+  line <- getScannerLine
+  liftIO $ report line "" message
+
+report :: Int -> Text -> Text -> IO ()
+report line whrErr message = do
+  let errMsg = "[line " <> show line <> "]" <> whrErr <> ": " <> message
+  hPutStrLn stderr (toString errMsg)
